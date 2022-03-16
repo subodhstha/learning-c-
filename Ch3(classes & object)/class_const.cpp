6e506033ff9fd7e497bd9b12492d1fa3fd84658f
Subodh Shrestha <shresthasubodh73@gmail.com>
2022-03-16 08:25:32 +0545

    cpp

--- Ch3(classes & object)/class_const.cpp
+++ Ch3(classes & object)/class_const.cpp
@@ -1,14 +1,26 @@
 #include<iostream>
+using namespace std;
 class rect
 {
     int l,b;
     public:
-        rect(int l, int b)
+        rect(int x, int y)
         {
-            // cout<<"Enter the value of length and breadth";
-            // cin>>l>>b;
-
+            l= x;
+            b = y;
         }
-    
-
+        int area()
+        {
+            return(l*b);
+        }
 };
+
+int main()
+{
+    int a, b;
+    cout<<"Enter the value of length and breadth"<<endl;
+    cin>>a>>b;
+    cout<<"The area of length "<<a<<" and breadth "<<b<<" are"<<endl;
+    rect r(a,b);
+    cout<<r.area();
+}
