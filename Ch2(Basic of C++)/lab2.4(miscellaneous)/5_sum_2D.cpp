2c15789e36ff9588ec2f33c0bbd202a9ddda262c
subodh <subodh@subodh.subodh>
2022-03-30 07:16:58 +0545

    cpp

--- Ch2(Basic of C++)/lab2.4(miscellaneous)/5_sum_2D.cpp
+++ Ch2(Basic of C++)/lab2.4(miscellaneous)/5_sum_2D.cpp
@@ -4,5 +4,47 @@ using namespace std;
 
 int main()
 {
-    
+    int r, c, a[10][10],b[10][10], sum[10][10];
+    cout<<"Enter the number of row and column"<<endl;
+    cin>>r>>c;
+    cout<<"Enter the data of first matrix"<<endl;
+    for(int i=0; i<r; i++)
+    {
+        for(int j=0; j<c; j++)
+        {
+            cout<<"Enter the element a"<<i+1<<j+1<<endl;
+            cin>>a[i][j];
+        }
+    }
+
+    cout<<"Enter the data of second matrix"<<endl;
+    for(int i=0; i<r; i++)
+    {
+        for(int j=0; j<c; j++)
+        {
+            cout<<"Enter the element b"<<i+1<<j+1<<endl;
+            cin>>b[i][j];
+        }
+    }
+
+    for(int i=0; i<r; i++)
+    {
+        for(int j=0; j<c; j++)
+        {
+            sum[i][j]= a[i][j] + b[i][j];
+        }
+    }
+
+    cout<<"The sum of the matrix is ";
+    for(int i=0; i<r; i++)
+    {
+        for(int j=0; j<c; j++)
+        {
+            cout<<sum[i][j]<<"   ";
+            if(j == c-1)
+            {
+                cout<<endl;
+            }
+        }
+    }
 }
